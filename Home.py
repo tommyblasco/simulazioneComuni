ce9ba0eb07b78b1ab46fc9787182ff43aace1c88
Thomas <sacchinit@gmail.com>
2023-12-26 13:34:58 +0100

    Add files via upload

--- Home.py
+++ Home.py
@@ -6,15 +6,9 @@ st.title("Campionati Comunali di Python")
 st.subheader("Il primo torneo simulato in Python, dove a sfidarsi sono i comuni italiani")
 
 st.header('Come funziona?🤔')
-st.text_area("","Le regioni italiane sono state suddivise in N gironi, con numero di comuni contigui partecipanti "
-                "quanto più simile alla Serie A italiana (20 squadre). "
-                "A questo scopo abbiamo utilizzato il k_means_constrained di Python. "
-                "Successivamente abbiamo creato un calendario di partite andata e ritorno stile serie A, "
-                "tra i comuni che compongono i vari gironi, a cui abbiamo assegnato un nome in base all'area della regione "
-                "interessata. "
-             "Infine abbiamo simulato l'intero campionato per stabilire la vincitrice del girone. "
-             "La simulazione del risultato è stata performata con un'estrazione causale da una distribuzione Poisson, "
-                "tenendo conto della forma delle 2 squadre pesata in modo decrescente (abbiamo assegnato più valore alle "
-                "partite più recenti). "
-                "I pesi decrescenti sono stati ottenuti tramite una funzione esponenziale negativa.")
-st.subheader("Per scoprire le classifiche finali, fai click nella sezione Campionati a sinistra!")
+st.text_area("","Ogni regione è stata suddivisa in gironi da 20 comuni circa ciascuno. "
+                "Ogni girone prevede un calendario di partite andata e ritorno all'italiana dove ogni comune "
+             "sfida gli altri. I risultati vengono simulati da Python. "
+             "Al termine del processo, viene calcolata una classifica finale.")
+st.info("Per scoprire le classifiche finali, fai click nella sezione Campionati a sinistra!")
+st.info("Per maggiori dettagli sul procedimento, fai click nella sezione Info a sinistra!")
